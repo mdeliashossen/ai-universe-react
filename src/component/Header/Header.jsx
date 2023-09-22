4059285bfc4a3ac485f24621e82806c8d51f6732
mdeliashossen <skeliascse@gmail.com>
2023-09-22 23:20:57 +0600

    title

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => {
     return (
         <>
-            <h1 className='header'>Welcome to AI Universe</h1>
+            <h1 className='header'>Welcome to AI New Thinks</h1>
         </>
     );
 };
